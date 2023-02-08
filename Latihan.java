ef3bf8308e504039b8f5aa3ce9772dce2b84fdc1
moch_alfan <mochalfannn@gmail.com>
2023-02-08 13:39:35 +0700

    Update Latihan.java

--- Latihan.java
+++ Latihan.java
@@ -3,7 +3,10 @@ package latihan;
 
 public class Latihan {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("===");
+        System.out.println("Mochamad Alfan Muawad");
+        System.out.println("Golongan B");
+        System.out.println("TIF");
     }
     
 }
